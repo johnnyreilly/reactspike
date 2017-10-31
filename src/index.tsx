6e43f51c14413b5f7c513d44d420ab58dbf1615c
John Reilly <johnny_reilly@hotmail.com>
2017-10-31 17:32:29 +0000

    remove hot loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
-import { AppContainer } from 'react-hot-loader';
 import { App } from './app';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/main.scss';
@@ -11,24 +10,13 @@ import './styles/main.scss';
  */
 function render(Component: React.SFC) {
     const rootEl = document.getElementById('root');
-    ReactDOM.render(
-        <AppContainer>
-            <Router>
-                <Component />
-            </Router>
-        </AppContainer>,
+    ReactDOM.hydrate(
+        <Router>
+            <Component />
+        </Router>,
         rootEl
     );
 }
 
 render(App);
 registerServiceWorker();
-
-// Hot Module Replacement API
-if ((module as any).hot) {
-    (module as any).hot.accept('./app', () => {
-        const makeNextApp = require('./app').default;
-        const nextApp = makeNextApp(['app']);
-        render(nextApp);
-    });
-}
