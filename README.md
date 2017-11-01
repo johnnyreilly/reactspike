5b75859f3644c3716f25715345b86b72fa5f2432
John Reilly <johnny_reilly@hotmail.com>
2017-11-01 20:46:11 +0000

    cache the template upfront

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/johnnyreilly/reactspike.svg?branch=master)](https://www.travis-ci.org/johnnyreilly/reactspike)
 
-Reacting the ReadSpike...  To access the app go here: https://johnnyreilly.github.io/reactspike/
+Reacting the ReadSpike...  To access the app go here: https://reactspike.azurewebsites.net/
 
 All changes to the repo are built on Travis and deployed to GitHub Pages.
 
--- src/server.tsx
+++ src/server.tsx
@@ -1,47 +1,52 @@
 // tslint:disable:no-console
 import { readFile } from 'fs';
 import * as path from 'path';
-import { promisify } from 'util';
 import * as React from 'react';
 import * as express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import { renderToNodeStream } from 'react-dom/server';
 import { App } from './app';
 
-const readFileAsync = promisify(readFile);
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3000;
 console.info(`Server starting in ${isDev ? 'dev mode' : 'prod mode'} on port ${PORT} in ${process.cwd()} __dirname: ${__dirname} ...`);
 
-const server = express();
-
-server.use(express.static(__dirname));
-
-server.get('/', async (req, res) => {
-    if (isDev) {
-        const { httpVersion, method, url } = req;
-        console.info(`${httpVersion} ${method} ${url}`);
+const templatePath = path.join(__dirname, 'template.html');
+readFile(templatePath, 'utf8', (err, indexHtml) => {
+    if (err) {
+        console.error('Problem loading ' + templatePath, err);
+        return;
     }
 
-    const indexHtml = await readFileAsync(path.join(__dirname, 'template.html'), 'utf8');
     const [indexHtmlStart, indexHtmlEnd] = indexHtml.split('Welcome to ReadSpike. Loading...');
-    res.write(indexHtmlStart);
-
-    const context = {};
-    const stream = renderToNodeStream(
-        <StaticRouter location={req.url} context={context}>
-            <App />
-        </StaticRouter>
-    );
-    stream.pipe(res, { end: false });
-
-    stream.on('end', () => {
-        res.write(indexHtmlEnd);
-        res.end();
+    const server = express();
+    
+    server.use(express.static(__dirname));
+    
+    server.get('/', async (req, res) => {
+        if (isDev) {
+            const { httpVersion, method, url } = req;
+            console.info(`${httpVersion} ${method} ${url}`);
+        }
+    
+        res.write(indexHtmlStart);
+    
+        const context = {};
+        const stream = renderToNodeStream(
+            <StaticRouter location={req.url} context={context}>
+                <App />
+            </StaticRouter>
+        );
+        stream.pipe(res, { end: false });
+    
+        stream.on('end', () => {
+            res.write(indexHtmlEnd);
+            res.end();
+        });
     });
+    
+    server.listen(PORT, isDev
+        ? () => console.info(`Serving up at http://localhost:${PORT} ...`)
+        : undefined
+    );        
 });
-
-server.listen(PORT, isDev
-    ? () => console.info(`Serving up at http://localhost:${PORT} ...`)
-    : undefined
-);
