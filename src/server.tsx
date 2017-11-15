c06f6173f377b810b68f065c1c01365f3c6f2af8
John Reilly <johnny_reilly@hotmail.com>
2017-11-15 05:57:19 +0000

    try to disable caching

--- src/server.tsx
+++ src/server.tsx
@@ -44,6 +44,10 @@ async function startServer({ templatePath }: { templatePath: string }) {
             }
 
             res.set('Content-Type', 'text/html');
+            res.setHeader('Surrogate-Control', 'no-store');
+            res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+            res.setHeader('Pragma', 'no-cache');
+            res.setHeader('Expires', '0');
             res.write(indexHtmlStart);
 
             const context = {};
